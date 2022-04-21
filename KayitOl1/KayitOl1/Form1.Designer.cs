27df4b0ea2c75062d1b7e50968ba3be9a3575f40
Selin Şahin <selinsahin.sh@gmail.com>
2022-04-21 15:07:52 +0300

    kullanıcı kayıt olma ekranı
    windows form ile kayıt olurken şifre test edilir

---
+++ KayitOl1/KayitOl1/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace KayitOl1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_kAdi = new System.Windows.Forms.Label();
+            this.lbl_sifre = new System.Windows.Forms.Label();
+            this.txt_kAdi = new System.Windows.Forms.TextBox();
+            this.txt_sifre = new System.Windows.Forms.TextBox();
+            this.btn_kayitOl = new System.Windows.Forms.Button();
+            this.lbl_minKarakter = new System.Windows.Forms.Label();
+            this.lbl_buyukHarf = new System.Windows.Forms.Label();
+            this.lbl_kucukHarf = new System.Windows.Forms.Label();
+            this.lbl_rakam = new System.Windows.Forms.Label();
+            this.lbl_sembol = new System.Windows.Forms.Label();
+            this.lstBox_puan = new System.Windows.Forms.ListBox();
+            this.lstBox_mesaj = new System.Windows.Forms.ListBox();
+            this.lstBox_genelPuan = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lbl_kAdi
+            // 
+            this.lbl_kAdi.AutoSize = true;
+            this.lbl_kAdi.Location = new System.Drawing.Point(9, 39);
+            this.lbl_kAdi.Name = "lbl_kAdi";
+            this.lbl_kAdi.Size = new System.Drawing.Size(166, 29);
+            this.lbl_kAdi.TabIndex = 0;
+            this.lbl_kAdi.Text = "Kullanıcı Adı: ";
+            // 
+            // lbl_sifre
+            // 
+            this.lbl_sifre.AutoSize = true;
+            this.lbl_sifre.Location = new System.Drawing.Point(12, 113);
+            this.lbl_sifre.Name = "lbl_sifre";
+            this.lbl_sifre.Size = new System.Drawing.Size(71, 29);
+            this.lbl_sifre.TabIndex = 0;
+            this.lbl_sifre.Text = "Şifre:";
+            // 
+            // txt_kAdi
+            // 
+            this.txt_kAdi.Location = new System.Drawing.Point(215, 36);
+            this.txt_kAdi.Name = "txt_kAdi";
+            this.txt_kAdi.Size = new System.Drawing.Size(224, 35);
+            this.txt_kAdi.TabIndex = 1;
+            this.txt_kAdi.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txt_sifre
+            // 
+            this.txt_sifre.Location = new System.Drawing.Point(215, 113);
+            this.txt_sifre.Name = "txt_sifre";
+            this.txt_sifre.Size = new System.Drawing.Size(224, 35);
+            this.txt_sifre.TabIndex = 2;
+            this.txt_sifre.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btn_kayitOl
+            // 
+            this.btn_kayitOl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btn_kayitOl.Font = new System.Drawing.Font("Corbel", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_kayitOl.ForeColor = System.Drawing.Color.Yellow;
+            this.btn_kayitOl.Location = new System.Drawing.Point(78, 177);
+            this.btn_kayitOl.Name = "btn_kayitOl";
+            this.btn_kayitOl.Size = new System.Drawing.Size(232, 72);
+            this.btn_kayitOl.TabIndex = 3;
+            this.btn_kayitOl.Text = "KAYIT OL";
+            this.btn_kayitOl.UseVisualStyleBackColor = false;
+            this.btn_kayitOl.Click += new System.EventHandler(this.btn_kayitOl_Click);
+            // 
+            // lbl_minKarakter
+            // 
+            this.lbl_minKarakter.AutoSize = true;
+            this.lbl_minKarakter.ForeColor = System.Drawing.Color.Red;
+            this.lbl_minKarakter.Location = new System.Drawing.Point(11, 269);
+            this.lbl_minKarakter.Name = "lbl_minKarakter";
+            this.lbl_minKarakter.Size = new System.Drawing.Size(330, 29);
+            this.lbl_minKarakter.TabIndex = 3;
+            this.lbl_minKarakter.Text = "En az 9 karakter olmalı. (10p)";
+            // 
+            // lbl_buyukHarf
+            // 
+            this.lbl_buyukHarf.AutoSize = true;
+            this.lbl_buyukHarf.ForeColor = System.Drawing.Color.Red;
+            this.lbl_buyukHarf.Location = new System.Drawing.Point(13, 313);
+            this.lbl_buyukHarf.Name = "lbl_buyukHarf";
+            this.lbl_buyukHarf.Size = new System.Drawing.Size(355, 29);
+            this.lbl_buyukHarf.TabIndex = 3;
+            this.lbl_buyukHarf.Text = "Büyük harf bulunmalı. (10/20p)";
+            // 
+            // lbl_kucukHarf
+            // 
+            this.lbl_kucukHarf.AutoSize = true;
+            this.lbl_kucukHarf.ForeColor = System.Drawing.Color.Red;
+            this.lbl_kucukHarf.Location = new System.Drawing.Point(13, 357);
+            this.lbl_kucukHarf.Name = "lbl_kucukHarf";
+            this.lbl_kucukHarf.Size = new System.Drawing.Size(355, 29);
+            this.lbl_kucukHarf.TabIndex = 3;
+            this.lbl_kucukHarf.Text = "Küçük harf bulunmalı. (10/20p)";
+            // 
+            // lbl_rakam
+            // 
+            this.lbl_rakam.AutoSize = true;
+            this.lbl_rakam.ForeColor = System.Drawing.Color.Red;
+            this.lbl_rakam.Location = new System.Drawing.Point(13, 396);
+            this.lbl_rakam.Name = "lbl_rakam";
+            this.lbl_rakam.Size = new System.Drawing.Size(311, 29);
+            this.lbl_rakam.TabIndex = 3;
+            this.lbl_rakam.Text = "Rakam bulunmalı. (10/20p)";
+            // 
+            // lbl_sembol
+            // 
+            this.lbl_sembol.AutoSize = true;
+            this.lbl_sembol.ForeColor = System.Drawing.Color.Red;
+            this.lbl_sembol.Location = new System.Drawing.Point(13, 436);
+            this.lbl_sembol.Name = "lbl_sembol";
+            this.lbl_sembol.Size = new System.Drawing.Size(276, 29);
+            this.lbl_sembol.TabIndex = 3;
+            this.lbl_sembol.Text = "Sembol bulunmalı. (10p)";
+            // 
+            // lstBox_puan
+            // 
+            this.lstBox_puan.BackColor = System.Drawing.Color.Black;
+            this.lstBox_puan.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lstBox_puan.Font = new System.Drawing.Font("Corbel", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstBox_puan.ForeColor = System.Drawing.Color.White;
+            this.lstBox_puan.FormattingEnabled = true;
+            this.lstBox_puan.ItemHeight = 39;
+            this.lstBox_puan.Location = new System.Drawing.Point(385, 301);
+            this.lstBox_puan.Name = "lstBox_puan";
+            this.lstBox_puan.Size = new System.Drawing.Size(286, 121);
+            this.lstBox_puan.TabIndex = 4;
+            this.lstBox_puan.SelectedIndexChanged += new System.EventHandler(this.lstBox_puan_SelectedIndexChanged);
+            // 
+            // lstBox_mesaj
+            // 
+            this.lstBox_mesaj.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lstBox_mesaj.FormattingEnabled = true;
+            this.lstBox_mesaj.ItemHeight = 29;
+            this.lstBox_mesaj.Location = new System.Drawing.Point(41, 488);
+            this.lstBox_mesaj.Name = "lstBox_mesaj";
+            this.lstBox_mesaj.Size = new System.Drawing.Size(651, 91);
+            this.lstBox_mesaj.TabIndex = 5;
+            // 
+            // lstBox_genelPuan
+            // 
+            this.lstBox_genelPuan.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lstBox_genelPuan.FormattingEnabled = true;
+            this.lstBox_genelPuan.ItemHeight = 29;
+            this.lstBox_genelPuan.Location = new System.Drawing.Point(41, 610);
+            this.lstBox_genelPuan.Name = "lstBox_genelPuan";
+            this.lstBox_genelPuan.Size = new System.Drawing.Size(651, 62);
+            this.lstBox_genelPuan.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(748, 764);
+            this.Controls.Add(this.lstBox_genelPuan);
+            this.Controls.Add(this.lstBox_mesaj);
+            this.Controls.Add(this.lstBox_puan);
+            this.Controls.Add(this.lbl_sembol);
+            this.Controls.Add(this.lbl_rakam);
+            this.Controls.Add(this.lbl_kucukHarf);
+            this.Controls.Add(this.lbl_buyukHarf);
+            this.Controls.Add(this.lbl_minKarakter);
+            this.Controls.Add(this.btn_kayitOl);
+            this.Controls.Add(this.txt_sifre);
+            this.Controls.Add(this.txt_kAdi);
+            this.Controls.Add(this.lbl_sifre);
+            this.Controls.Add(this.lbl_kAdi);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Kayit Ol";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_kAdi;
+        private System.Windows.Forms.Label lbl_sifre;
+        private System.Windows.Forms.TextBox txt_kAdi;
+        private System.Windows.Forms.TextBox txt_sifre;
+        private System.Windows.Forms.Button btn_kayitOl;
+        private System.Windows.Forms.Label lbl_minKarakter;
+        private System.Windows.Forms.Label lbl_buyukHarf;
+        private System.Windows.Forms.Label lbl_kucukHarf;
+        private System.Windows.Forms.Label lbl_rakam;
+        private System.Windows.Forms.Label lbl_sembol;
+        private System.Windows.Forms.ListBox lstBox_puan;
+        private System.Windows.Forms.ListBox lstBox_mesaj;
+        private System.Windows.Forms.ListBox lstBox_genelPuan;
+    }
+}
+
---
+++ KayitOl1/KayitOl1/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;                               //sistemi kullanmak icin gereken kutuphane
+using System.Collections.Generic;           //sistemin genel koleksiyonlarini kullanma icin gereken kutuphane
+using System.ComponentModel;                //sistem bilesenlerini kullanma icin gereken kutuphane
+using System.Data;                          //sistem verisi kullanilir
+using System.Drawing;                       //sistemi cizer
+using System.Linq;                          //veri sorgulamak icin kullanilir
+using System.Text;                          //metin icin kullanilir
+using System.Threading.Tasks;               //is parcacigi gorevlerini aktif hale getiren kutuphane
+using System.Windows.Forms;                 //windows form kullanma icin gereken kutuphane
+
+namespace KayitOl1                          //kayitOl isimli isim uzayi
+{
+    public partial class Form1 : Form        //genele acik Form1 icerisindeki Form kismi sinifi
+    {
+        public Form1()                       //genele acik Form1 metodu
+        {
+            InitializeComponent();          //Form1 nesnesine ait olan uye elemanlarla ilgili ilk islemler yapilir.
+        }
+
+        bool dokuzKarakter;                 //kontrol islemi icin karakter sayisi tanimlanir           
+        bool birBuyuk;                      //buyuk harf kontrolu icin anahtar tanimlama
+        bool birKucuk;                      //kucuk harf kontrolu icin anahtar tanimlama
+        bool birRakam;                      //rakam kontrolu icin anahtar tanimlama
+        bool birSembol;                     //sembol kontrolu icin anahtar tanimlama
+
+        private void textBox1_TextChanged(object sender, EventArgs e)   //textbox1 i baslatan ozel fonksiyon
+        {
+            
+           
+        }
+        private void Form1_Load(object sender, EventArgs e)               //form1 yuklemesini baslatan fonksiyon
+        {
+            
+        }
+
+        private void btn_kayitOl_Click(object sender, EventArgs e)          //kayitol butonuna tiklaninca gerceklesek olaylar
+        {
+            dokuzKarakter = false;      //karakter sayisi yanlis olarak tanimlandi
+            birBuyuk = false;      //buyuk harf yanlis olarak tanimlandi
+            birKucuk = false;      //kucuk harf yanlis olarak tanimlandi
+            birRakam = false;      //rakam yanlis olarak tanimlandi
+            birSembol = false;      //sembol yanlis olarak tanimlandi
+
+            int genelSayac = 0;          //genel sayac 0'a esitlendi
+            int karakterSayaci = 0;          //karakter sayaci 0'a esitlendi
+            int buyukHarfSayaci = 0;          //buyuk harf sayisi 0'a esitlendi
+            int buyukHarfPuani = 0;          //buyuk harf puani 0'a esitlendi
+            int kucukHarfSayaci = 0;          //kucuk harf sayisi 0'a esitlendi
+            int kucukHarfPuani = 0;          //kucuk harf puani 0'a esitlendi
+            int rakamSayaci = 0;          //rakam sayisi 0'a esitlendi
+            int rakamPuani = 0;          //rakam puani 0'a esitlendi
+            int sembolSayaci = 0;          //sembol sayisi 0'a esitlendi
+            int sembolPuani = 0;          //sembol puani 0'a esitlendi
+
+            lstBox_mesaj.Items.Clear();                     //mesaj listboxundaki veriler temizlendi
+
+            if (txt_sifre.Text.ToCharArray().Count()>0)         //sifrenin bos olmadigi kontrol edilir
+            {
+                char[] karakterler = txt_sifre.Text.ToCharArray();  //karakterler degiskeni sifrenin sayisina esitlenir
+                karakterSayisi();       //karakter sayisi fonksiyonu cagrilir
+                void karakterSayisi()       //karakter sayisi fonksiyonu olusturulur
+                {
+                    if(karakterler.Length>=9)       //karakter uzunlugu 9'a esit ve buyukse buraya girer
+                    {
+                        dokuzKarakter = true;       //dokuz karakter anahtari dogruya cevrilir
+                        lbl_minKarakter.ForeColor = Color.Green;        //minimum karakter label'i yesil renk olur
+                        karakterSayaci = 10;            //karakter sayaci 10 olur
+                    }
+                    else                                        //(if saglanmazsa) karakter sayisi 9dan kucukse buraya girer
+                    {
+                        dokuzKarakter = false;                    //dokuz karakter anahtari yanlisa cevrilir
+                        lbl_minKarakter.ForeColor = Color.Red;    //minimum karakter label'i kirmizi renk olur
+                        karakterSayaci = 0;                         //karakter sayaci 0 olur
+                        lstBox_mesaj.Items.Add("Parola en az 9 karakterden oluşmalıdır.");         //mesaj kutusuna saglanmasi gereken kosul yazdirilir
+                    }
+                    buyukKontrol();                     //buyuk harf kontrol fonksiyonu cagrilir
+                }
+
+                
+                void buyukKontrol()                     //buyuk harf kontrol fonksiyonu olusturulur
+                {
+                    foreach (char karakter in karakterler)      //foreach dongusuyle diziler icindeki degerler kullanilir
+                    {
+                        if (char.IsUpper(karakter))         //buyuk harf karakter sayisi kadar dongu doner
+                        {
+                            birBuyuk = true;                //buyuk harf anahtarini true yapar
+                            buyukHarfSayaci += 1;           //buyuk harf sayacini 1 arttirir
+                        }
+                        if(buyukHarfSayaci==1)              //buyuk harf sayisi 1 ise buraya girer
+                        {
+                            buyukHarfPuani = 10;            //buyuk harf puanini 10a esitler
+                        }
+                        else if(buyukHarfSayaci>1)          //buyuk harf sayaci 1den buyukse buraya girer
+                        {
+                            buyukHarfPuani = 20;            //buyuk harf puani 20 olur
+                        }
+                        else                                //diger kosullar saglanmazsa buraya girer
+                        {
+                            buyukHarfPuani = 0;             //buyuk harf puanini 0 yapar
+                        } 
+                    }
+                    if (birBuyuk == true)                   //buyuk harf anahtari dogru ise buraya girer
+                    {
+                        lbl_buyukHarf.ForeColor = Color.Green;          //buyuk harf kosulu yazisini yesil renk yapar
+                    }
+                    else                                                //diger kosul saglanmazsa
+                    {
+                        lbl_buyukHarf.ForeColor = Color.Red;            //buyuk harf kosulu yazisini kirmizi renk yapar
+                        lstBox_mesaj.Items.Add("Parolanız büyük harf içermelidir.");    //mesaj listboxina gerekli uyari mesaji yazdirilir
+                    }
+                    kucukKontrol();                                         //kucuk harf kontrol fonksiyonu cagrilir
+                }
+
+                
+                void kucukKontrol()                     //kucuk harf kontrol fonksiyonu olusturulur
+                {
+                    foreach (char karakter in karakterler)      //foreach dongusuyle diziler icindeki degerler kullanilir
+                    {
+                        if (char.IsLower(karakter))          //kucuk harf karakter sayisi kadar dongu doner    
+                        {
+                            birKucuk = true;               //kucuk harf anahtarini true yapar
+                            kucukHarfSayaci += 1;           //kucuk harf sayacini 1 arttirir
+                        }
+                        if(kucukHarfSayaci==1)              //kucuk harf sayisi 1 ise buraya girer
+                        {
+                            kucukHarfPuani = 10;           //kucuk harf puanini 10a esitler
+                        }
+                        else if(kucukHarfSayaci>1)          //kucuk harf sayaci 1den buyukse buraya girer
+                        {
+                            kucukHarfPuani = 20;            //kucuk harf puani 20 olur
+                        }
+                        else                               //diger kosullar saglanmazsa buraya girer
+                        {
+                            kucukHarfPuani = 0;            //kucuk harf puanini 0 yapar
+                        }
+                    }
+                    if (birKucuk == true)                  //kucuk harf anahtari dogru ise buraya girer
+                    {
+                        lbl_kucukHarf.ForeColor = Color.Green;         //kucuk harf kosulu yazisini yesil renk yapar
+                    }
+                    else                                               //diger kosul saglanmazsa
+                    {
+                        lbl_kucukHarf.ForeColor = Color.Red;            //kucuk harf kosulu yazisini kirmizi renk yapar
+                        lstBox_mesaj.Items.Add("Parolanız küçük harf içermelidir.");   //mesaj listboxina gerekli uyari mesaji yazdirilir
+                    }
+                    rakamKontrol();                                        //rakam kontrol fonksiyonu cagrilir
+                }
+
+               
+                void rakamKontrol()                     //rakam kontrol fonksiyonu olusturulur
+                {
+                    foreach(char karakter in karakterler)      //foreach dongusuyle diziler icindeki degerler kullanilir
+                    {
+                        if(char.IsNumber(karakter))          //rakam karakteri sayisi kadar dongu doner  
+                        {
+                            birRakam = true;             //rakam anahtarini true yapar
+                            rakamSayaci += 1;          //rakam sayacini 1 arttirir
+                        }
+                        if (rakamSayaci == 1)              //rakam sayisi 1 ise buraya girer
+                        {
+                            rakamPuani = 10;           //rakam puanini 10a esitler
+                        }
+                        else if(rakamSayaci>1)         //rakam sayaci 1den buyukse buraya girer
+                        {
+                            rakamPuani = 20;            //rakam puani 20 olur
+                        }
+                        else                               //diger kosullar saglanmazsa buraya girer
+                        {
+                            rakamPuani = 0;           //rakam puanini 0 yapar
+                        }
+                    }
+                    if(birRakam==true)                 //rakam anahtari dogru ise buraya girer
+                    {
+                        lbl_rakam.ForeColor = Color.Green;         //rakam kosulu yazisini yesil renk yapar
+                    }
+                    else                                              //diger kosul saglanmazsa
+                    {
+                        lbl_rakam.ForeColor = Color.Red;            //rakam kosulu yazisini kirmizi renk yapar
+                        lstBox_mesaj.Items.Add("Parolanız rakam içermelidir.");     //mesaj listboxina gerekli uyari mesaji yazdirilir
+                    }
+                    sembolKontrol();                                       //sembol kontrol fonksiyonu cagrilir
+                }
+
+                
+                void sembolKontrol()                      //sembol kontrol fonksiyonu olusturulur       
+                {
+                    foreach (char karakter in karakterler)      //foreach dongusuyle diziler icindeki degerler kullanilir
+                    {
+                        if (char.IsSymbol(karakter))          //sembol karakter sayisi kadar dongu doner
+                        {
+                            birSembol = true;             //sembol anahtarini true yapar
+                            sembolSayaci += 1;          //sembol harf sayacini 1 arttirir
+                        }
+                        if(sembolSayaci==1)              //sembol sayisi 1 ise buraya girer
+                        {
+                            sembolPuani = 10;           //sembol harf puanini 10a esitler
+                        }
+                        else if(sembolSayaci>1)        //sembol sayaci 1den buyukse buraya girer
+                        { 
+                            sembolPuani += 10;           //sembol puani 20 olur
+                        }
+                        else                               //diger kosullar saglanmazsa buraya girer
+                        {
+                            sembolPuani = 0;          //sembol puanini 0 yapar
+                        }
+                    }
+                    if (birSembol == true)                 //sembol anahtari dogru ise buraya girer
+                    {
+                        lbl_sembol.ForeColor = Color.Green;        //sembol kosulu yazisini yesil renk yapar
+                    }
+                    else                                              //diger kosul saglanmazsa
+                    {
+                        lbl_sembol.ForeColor = Color.Red;           //sembol kosulu yazisini kirmizi renk yapar
+                        lstBox_mesaj.Items.Add("Parolanız sembol içermelidir.");    //mesaj listboxina gerekli uyari mesaji yazdirilir
+                    }
+                }
+
+            }
+
+            genelSayac = karakterSayaci + buyukHarfPuani + kucukHarfPuani + rakamPuani + sembolPuani;       
+            //genel sayac puanini diger tum puanlarin toplamina esitler
+            lstBox_puan.Items.Clear();      //puan listbox verilerini temizler
+            lstBox_puan.Items.Add("Genel Puan: " + genelSayac);     //puan listbox'ina genel puan yazdirilir
+
+            lstBox_genelPuan.Items.Clear();     //genel puan listboxi temizlenir
+
+            if(dokuzKarakter==true && birBuyuk==true && birKucuk == true && birRakam == true && birSembol == true)  //tum kosullar dogru saglaniyorsa donguye girer
+            { 
+                if (genelSayac>=90)     //genel sayac 90a esit ve buyukse
+                {
+                    lstBox_genelPuan.Items.Add("Şifreniz güçlüdür.");       //puan listboxina sifreniz gucludur yazdirilir
+                }
+                else if(genelSayac>=70 && genelSayac<90)    //genel puan 79-90 arasi ise
+                {
+                    lstBox_genelPuan.Items.Add("Şifreniz uygundur.");     //puan listboxina sifreniz uygundur yazdirilir
+                }
+                else                                                    //diger kosullar saglanmaz ise buraya girer
+                {
+                    lstBox_genelPuan.Items.Add("Şifrenizin puanı 70'ten küçük olduğu için kayıt olamazsınız.");     //genel puan listbox uyesine uyari mesaji yazdirilir
+                    MessageBox.Show("Şifrenizin puanı 70'ten küçük olduğu için kayıt olamazsınız.");        //ekrana uyari niteliginde mesaj kutusu gonderilir
+                }
+                lstBox_mesaj.Items.Add("Tüm koşulları sağladınız.");        //listboxa yazi yazdirilir
+                
+                foreach (var item in this.Controls)     //foreach dongusuyle diziler icindeki degerler kullanilir
+                {
+                      if (item.GetType() == typeof(ListBox))        //listbox uyesi
+                      {
+                         ((ListBox)item).BackColor = Color.Green;   //listbox arka plan rengini yesil yapar
+                      }
+                 }
+                        
+            }
+            else                                                                 //kosullar saglanmazsa buraya girer
+            {
+                foreach (var item in this.Controls)                     //foreach dongusuyle diziler icindeki degerler kullanilir
+                {
+                    if (item.GetType() == typeof(ListBox))              //listbox uyesi
+                    {
+                        ((ListBox)item).BackColor = Color.Red;          //listbox arka plan rengini kirmizi yapar
+                    }
+                }
+                MessageBox.Show("Şifreniz uygun değildir.");            //ekrana uyari niteliginde sifreniz uygun degildir mesaj kutusu yazdirilir
+                lstBox_genelPuan.Items.Add("Şifreniz uygun değildir.");     //genel puan listboxina uyari yazisi yazdirilir         
+            }
+        }
+
+        private void lstBox_puan_SelectedIndexChanged(object sender, EventArgs e)   //secilen dizinin degisimi olan ozel fonksiyonu
+        {
+
+        }
+    }
+}
